ce3ed9d442a5003e3663b3c7239e2d42ae200ca3
KeziaOliveira <kezia2709@gmail.com>
2024-11-10 19:23:17 -0300

    feat: Implemented User POST

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+// Remove as rotas definidas pelo WordPress
+remove_action('rest_api_init', 'create_initial_rest_routes', 99);
+
+// Linca a página do user no endpoints
+$dirbase = get_template_directory();
+require_once $dirbase . '/endpoints/user_post.php';
+
+// Modifica o prefixo da API de wp-json para json apenas
+// Necessário salvar os permalinks para dar um refresh nos URL's
+function change_api($slug) {
+  return 'json';
+}
+add_filter('rest_url_prefix', 'change_api');
+add_action('rest_api_init', function () {
+  $routes = rest_get_server()->get_routes();
+  error_log(print_r($routes, true));
+});
+
+?>
