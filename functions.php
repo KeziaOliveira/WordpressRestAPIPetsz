2482bfffa052fbea8bc3080dbca18ca27e8c2bfe
KeziaOliveira <kezia2709@gmail.com>
2024-11-20 22:56:16 -0300

    feat: REST API configurated

--- functions.php
+++ functions.php
@@ -2,6 +2,13 @@
 // Remove as rotas definidas pelo WordPress
 // remove_action('rest_api_init', 'create_initial_rest_routes', 99);
 
+// Remove endpoins específicos no wordpress por questão de segurança
+add_filter('rest_endpoints', function ($endpoints) {
+  unset($endpoints['/wp/v2/users']);
+  unset($endpoints['/wp/v2/users/(?P<id>[\d]+)']);
+  return $endpoints;
+});
+
 // Linca a página do user no endpoints
 $dirbase = get_template_directory();
 require_once $dirbase . '/endpoints/user_post.php';
@@ -9,6 +16,14 @@ require_once $dirbase . '/endpoints/user_get.php';
 
 require_once $dirbase . '/endpoints/photo_delete.php';
 require_once $dirbase . '/endpoints/photo_post.php';
+require_once $dirbase . '/endpoints/photo_get.php';
+
+require_once $dirbase . '/endpoints/comment_post.php';
+require_once $dirbase . '/endpoints/comment_get.php';
+
+require_once $dirbase . '/endpoints/password.php';
+
+require_once $dirbase . '/endpoints/stats_get.php';
 
 // Torna imagem em tamanho padrão
 update_option('large_size_w', 1000);
@@ -17,14 +32,10 @@ update_option('large_crop', 1);
 
 // Modifica o prefixo da API de wp-json para json apenas
 // Necessário salvar os permalinks para dar um refresh nos URL's
-function change_api($slug) {
+function change_api() {
   return 'json';
 }
-add_filter('rest_url_prefix', 'change_api');
-add_action('rest_api_init', function () {
-  $routes = rest_get_server()->get_routes();
-  error_log(print_r($routes, true));
-});
+add_filter('rest_url_prefix', 'change_api',);
 
 // Tempo em que expira o token do JWT (No caso em 24h) - Assim o usuário precisa fazer o login no tempo determinado.
 // function expire_token() {
